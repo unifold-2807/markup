3d4d6604d682ccd5f7b5a98b8d7f01cbdd2468ac
unifold-2807 <unifold28@gmail.com>
2025-01-02 18:51:57 +0100

    Add lists lexer tests

---
+++ source/tests/test_ordered_list.py
@@ -0,0 +1,64 @@
+import source.tests.template as template
+
+data = [
+    {
+        "name": "single_item",
+        "source": "+ An ordered list item",
+        "tokens": [
+            ["HYPHEN"],
+            ["TEXT", "An unordered list item"],
+        ]
+    },
+    {
+        "name": "multiple_items",
+        "source": "+ An ordered list item 1\n+ An ordered list item 2\n+ An ordered list item 3",
+        "tokens": [
+            ["PLUS"],
+            ["TEXT", "An ordered list item 1"],
+            ["NEWLINE"],
+            ["PLUS"],
+            ["TEXT", "An ordered list item 2"],
+            ["NEWLINE"],
+            ["PLUS"],
+            ["TEXT", "An ordered list item 3"],
+        ]
+    },
+    {
+        "name": "inline_formattings",
+        "source": "+ An ordered list item with inline formattings: *bold*, /italicized/, _underlined_, ~struck through~, `inline code`",
+        "tokens": [
+            ["PLUS"],
+            ["TEXT", "An ordered list item with inline formattings: "],
+            ["ASTERISK"],
+            ["TEXT", "bold"],
+            ["ASTERISK"],
+            ["TEXT", ", "],
+            ["SLASH"],
+            ["TEXT", "italicized"],
+            ["SLASH"],
+            ["TEXT", ", "],
+            ["UNDERSCORE"],
+            ["TEXT", "underlined"],
+            ["UNDERSCORE"],
+            ["TEXT", ", "],
+            ["TILDE"],
+            ["TEXT", "struck through"],
+            ["TILDE"],
+            ["TEXT", ", "],
+            ["BACKTICK"],
+            ["TEXT", "inline code"],
+            ["BACKTICK"]
+        ]
+    },
+    {
+        "name": "marks",
+        "source": "+ An ordered list item with marks: #, |, ```, -, +",
+        "tokens": [
+            ["PLUS"],
+            ["TEXT", "An ordered list item with marks: #, |, ```, -, +"]
+        ]
+    }
+]
+
+class TestOrderedList(template.TestLexer): pass
+template.generate_tests(TestOrderedList, data)
---
+++ source/tests/test_unordered_list.py
@@ -0,0 +1,64 @@
+import source.tests.template as template
+
+data = [
+    {
+        "name": "single_item",
+        "source": "- An unordered list item",
+        "tokens": [
+            ["HYPHEN"],
+            ["TEXT", "An unordered list item"],
+        ]
+    },
+    {
+        "name": "multiple_items",
+        "source": "- An unordered list item 1\n- An unordered list item 2\n- An unordered list item 3",
+        "tokens": [
+            ["HYPHEN"],
+            ["TEXT", "An unordered list item 1"],
+            ["NEWLINE"],
+            ["HYPHEN"],
+            ["TEXT", "An unordered list item 2"],
+            ["NEWLINE"],
+            ["HYPHEN"],
+            ["TEXT", "An unordered list item 3"],
+        ]
+    },
+    {
+        "name": "inline_formattings",
+        "source": "- An unordered list item with inline formattings: *bold*, /italicized/, _underlined_, ~struck through~, `inline code`",
+        "tokens": [
+            ["HYPHEN"],
+            ["TEXT", "An unordered list item with inline formattings: "],
+            ["ASTERISK"],
+            ["TEXT", "bold"],
+            ["ASTERISK"],
+            ["TEXT", ", "],
+            ["SLASH"],
+            ["TEXT", "italicized"],
+            ["SLASH"],
+            ["TEXT", ", "],
+            ["UNDERSCORE"],
+            ["TEXT", "underlined"],
+            ["UNDERSCORE"],
+            ["TEXT", ", "],
+            ["TILDE"],
+            ["TEXT", "struck through"],
+            ["TILDE"],
+            ["TEXT", ", "],
+            ["BACKTICK"],
+            ["TEXT", "inline code"],
+            ["BACKTICK"]
+        ]
+    },
+    {
+        "name": "marks",
+        "source": "- An unordered list item with marks: #, |, ```, -, +",
+        "tokens": [
+            ["HYPHEN"],
+            ["TEXT", "An unordered list item with marks: #, |, ```, -, +"]
+        ]
+    }
+]
+
+class TestUnorderedList(template.TestLexer): pass
+template.generate_tests(TestUnorderedList, data)
